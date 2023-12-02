86cfcbbb368fa2f91440baf0fd0ad924b371aac9
marshot <124557537+marshot9420@users.noreply.github.com>
2023-12-02 22:17:02 +0900

    [Configs] css-loader 추가

--- webpack.config.ts
+++ webpack.config.ts
@@ -62,6 +62,10 @@ const config: IConfiguration = {
         },
         exclude: path.resolve(__dirname, 'node_modules'),
       },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
