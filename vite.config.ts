4d6079e57b5fe030c7f9e2279d9a275eae1d5241
marshot <124557537+marshot9420@users.noreply.github.com>
2023-12-05 14:50:28 +0900

    [Feat] 환경별 실행 및 개발 서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
 import { defineConfig } from 'vite';
 
+import dotenv from 'dotenv';
+import path from 'path';
+
 import react from '@vitejs/plugin-react';
+
+const DEV_ENV = 'development';
+const PROD_ENV = 'production';
+const isDevMode = process.env.NODE_ENV !== PROD_ENV;
+
+dotenv.config({ path: isDevMode ? '.env.development' : 'env.production' });
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_PREFIX: string = process.env.REACT_APP_API_PREFIX || '/';
+const PORT = Number(process.env.REACT_APP_PORT);
+
+const SOURCE_DIR = 'src';
+
+const ENV: { [x: string]: string } = {
+  NODE_ENV: isDevMode ? DEV_ENV : PROD_ENV,
+};
+
+for (const key in process.env) {
+  if (key && key.startsWith('REACT_APP_')) {
+    ENV[key] = process.env[key] || '';
+  }
+}
 
 export default defineConfig({
   plugins: [react()],
+  mode: isDevMode ? DEV_ENV : PROD_ENV,
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, SOURCE_DIR),
+    },
+  },
+  server: {
+    open: true,
+    port: PORT,
+    proxy: {
+      [API_PREFIX]: {
+        target: API_BASE_URL,
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(new RegExp(`^${API_PREFIX}`), ''),
+      },
+    },
+  },
 });
